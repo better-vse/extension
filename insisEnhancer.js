4fa80da5d37a7166ca291b2b24beccbf66b370bc
Matěj Lajtkep <matej.lajtkep@gmail.com>
2022-07-11 22:37:15 +0200

    close button

--- insisEnhancer.js
+++ insisEnhancer.js
@@ -4,16 +4,21 @@ function sleep(ms) {
 
 function init(){
     const $wrapper = $(`<div id="cp-wrapper" class="closed">
+        <div id="cp-closer">-</div>
     <div>`)
 
     const $opener = $(`<div id="cp-opener">+<div>`)
-
     $(document.body).append($wrapper)
     $(document.body).append($opener)
+    const $closer = $("#cp-closer")
 
     $opener.on("click", function(){
         $opener.hide();
         $wrapper.show()
+    })
+    $closer.on("click", function(){
+        $opener.show();
+        $wrapper.hide()
     })
 }
 
